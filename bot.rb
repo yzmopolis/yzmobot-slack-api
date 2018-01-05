a6bfde43a7b1042da8a86ca056092e36bdfbe24b
ika.grabon <ika.grabon@gmail.com>
2018-01-05 20:28:00 +0100

    Add eventmachine and websocket

--- bot.rb
+++ bot.rb
@@ -1,10 +1,38 @@
 require 'http'
 require 'json'
+require 'eventmachine'
+require 'faye/websocket'
 
-rc = HTTP.post("https://slack.com/api/chat.postMessage", params: {
+rc = HTTP.post("https://slack.com/api/rtm.start", params: {
     token: ENV['SLACK_API_TOKEN'],
-    channel: '#general',
-    text: "That is the last time we're taking directions from a SQUIRREL!!",
-    as_user: true
+    # channel: '#general',
+    # text: "That is the last time we're taking directions from a SQUIRREL!!",
+    # as_user: true
 })
-puts JSON.pretty_generate(JSON.parse(rc.body))
+
+rc = JSON.parse(rc.body)
+# puts JSON.pretty_generate(JSON.parse(rc.body))
+# puts rc['url'] omg it so weird
+url = rc['url']
+
+EM.run do
+  ws = Faye::WebSocket::Client.new(url)
+  ws.on :open do
+    p [:open]
+  end
+  ws.on :message do |event|
+    p [:message, JSON.parse(event.data)]
+    data = JSON.parse(event.data)
+    if data['text'] == 'yzma'
+      ws.send({ type:'message',
+              text: "Pull The Lever <@#{data['user']}>!",
+              channel: data['channel']}.to_json)
+    end
+  end
+  ws.on :close do
+    p [:close, event.code, event.reason]
+    ws = nil
+    EM.stop
+  end
+
+end
